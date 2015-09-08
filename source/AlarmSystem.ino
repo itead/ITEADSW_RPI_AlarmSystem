761b20ba27a139998caf76e5373ba995d8073aeb
xianminghuang <xianming.huang@itead.cc>
2015-09-08 17:52:34 +0800

    change variable name
    Signed-off-by: xianminghuang <xianming.huang@itead.cc>

--- source/AlarmSystem.ino
+++ source/AlarmSystem.ino
@@ -12,113 +12,87 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-
-
-
-
 #define GSM_POWER_PIN 11
 #define STATUS_ALARM GPIO4
-
+#define TELEPHONE_NUMBER "13169939943"
 SIMCOM900 gsm(GSM_POWER_PIN,Serial0);
 SMSGSM sms(gsm);
 CallGSM call(gsm);
-
 boolean started=false;
-
 byte ret;
 char DTMF_char='_';
-pid_t fd;
-pid_t md;
+pid_t pid;
+pid_t pppid;
 pid_t ppid;
 int cfd;
-int recbyte;
-int sin_size;
 char buffer[1024] = {0};
-struct sockaddr_in s_add, c_add;
+struct sockaddr_in s_add;
 unsigned short portnum = 0x8888;
-
-
-
-
 
 void setup(void)
 {
-     //Serial connection.
-     pinMode(STATUS_ALARM,INPUT);
-     printf("GSM Shield Call Test\n");
-     printf("Start...\n");
-     if (gsm.begin(9600)) 
-     {
-          printf("\nstatus=READY\n");
-          started=true;
-     } 
-     else
-     { 
+    pinMode(STATUS_ALARM,INPUT);
+    printf("Alarm system initialization\n");
+    printf("Start...\n");
+    if (gsm.begin(9600)) 
+    {
+      printf("\nstatus=READY\n");
+      started=true;
+    } 
+    else
+    { 
         printf("\nstatus=IDLE\n");
-     }
-     if(!started) 
-     {
+    }
+    if(!started) 
+    {
         printf("setup error,Halt now...");
         while(1)delay(10000);
-     }
-     
-     cfd = socket(AF_INET, SOCK_STREAM, 0);
-     if(-1 == cfd)
-     {
-             printf("socket fail ! \r\n");
-             //return -1;
-     }
- 
-     printf("socket ok !\r\n");
- 
-     bzero(&s_add,sizeof(struct sockaddr_in));
-     s_add.sin_family=AF_INET;
-     s_add.sin_addr.s_addr= inet_addr("172.16.5.154");
-     s_add.sin_port=htons(portnum);
-     printf("s_addr = %#x ,port : %#x\r\n",s_add.sin_addr.s_addr,s_add.sin_port);
- 
-     if(-1 == connect(cfd,(struct sockaddr *)(&s_add), sizeof(struct sockaddr)))
-     {
-             printf("connect fail !\r\n");
-             //return -1;
-     }
- 
-     printf("connect ok !\r\n");
-	
-    
+    }
+
+    cfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(-1 == cfd)
+    {
+         printf("socket fail ! \r\n");
+    }
+    bzero(&s_add,sizeof(struct sockaddr_in));
+    s_add.sin_family=AF_INET;
+    s_add.sin_addr.s_addr= inet_addr("172.16.5.154");
+    s_add.sin_port=htons(portnum);
+    printf("s_addr = %#x ,port : %#x\r\n",s_add.sin_addr.s_addr,s_add.sin_port);
+
+    if(-1 == connect(cfd,(struct sockaddr *)(&s_add), sizeof(struct sockaddr)))
+    {
+         printf("connect fail !\r\n");
+    }
 };
 
 void loop(void)
-
 {   
-    int times = 0;
-    int val = 1;
-    int timer = 40;
-    int log = 0; 
-    int press = 0; 
-    int fork_log = 1;
+    int talk_time = 0;
+    int ring_time = 50;
+    int connect_logo = 1;
     printf("Please press emergency call button\n");
     if (0 == digitalRead(STATUS_ALARM))
     {
         delay(5);  
         if (0 == digitalRead(STATUS_ALARM))
         {
-            call.Call("13169939943");
+            call.Call(TELEPHONE_NUMBER);
             call.SetDTMF(1);
             ret = call.CallStatus();
-            while (timer > 0)
+            while (ring_time > 0)
             {
                 ret = call.CallStatus();
-                printf("%d\n",ret);
-                timer--;
+                printf("%d\n",ret); /* 4 is dial, 3 is ringing and 10 is Connected */
+                ring_time--;
                 delay(1000);
                 
-                if( ret = call.CallStatus() == 10 && fork_log == 1 )
+                if( ret = call.CallStatus() == 10 && connect_logo == 1 )
                 {	
-                    fork_log = 0;
-                    timer = 0;
-                    fd = fork();
-                    if (fd == 0)
+                    connect_logo = 0;
+                    ring_time = 0;
+                    pid = fork();
+                    if (pid == 0)
                     {
                         while(1)
                         {
@@ -134,16 +108,14 @@ void loop(void)
                             while (1)
                             {
                                 DTMF_char = call.DetDTMF();
-                                ret = call.CallStatus();
-                                printf("%d\n",ret);
                                 if (DTMF_char == '1')
                                 {
-                                    int kill_ret = kill(fd ,SIGKILL);
+                                    int kill_ret = kill(pid ,SIGKILL);
                                     if (!kill_ret)
                                     {                        
                                         printf("\nYou press 1 xxxxxx\n");
-                                        md = fork();
-                                        if (md == 0)
+                                        pppid = fork();
+                                        if (pppid == 0)
                                         {
                                             while(1)
                                            {
@@ -162,12 +134,12 @@ void loop(void)
                                 }
                                 else if (DTMF_char == '2')
                                 {
-                                    int kill_ret = kill(fd ,SIGKILL);
+                                    int kill_ret = kill(pid ,SIGKILL);
                                     if (!kill_ret)
                                     {                        
                                         printf("\nYou press 2 xxxxxx\n");
-                                        md = fork();
-                                        if (md == 0)
+                                        pppid = fork();
+                                        if (pppid == 0)
                                         {
                                             while(1)
                                            {
@@ -187,38 +159,36 @@ void loop(void)
                                 else if (ret = call.CallStatus() == 0)
                                 {
                                     
-                                    kill(fd ,SIGKILL);
-                                    kill(getpid() ,SIGKILL); 
-                                    call.HangUp();
+                                    kill(pid ,SIGKILL);
+                                    exit(12);
+                                    
                                 }
                                 else 
                                 {
                                     delay(10);
-                                    times=times+1;
-                                    printf("%d\n",times);
-                                    if (times > 10)
+                                    talk_time=talk_time+1;
+                                    if (talk_time > 12)
                                     {
                                         printf("\n Will be automatically suspended \n");
-                                        kill(fd ,SIGKILL);
-                                        kill(getpid() ,SIGKILL);
-                                        call.HangUp();
+                                        kill(pid ,SIGKILL);
+                                        exit(12);
                                     }
                                 }
                             }
                         }
                     }
-                   
                     waitpid(ppid, NULL,0);
                     call.HangUp();
                 }
                 else if (ret = call.CallStatus() == 0)
                 {
-                    timer = 0;
+                    ring_time = 0;
                 }
             }
-            if(ret = call.CallStatus() == 0 && fork_log == 1)
+            /* Send message */
+            if(ret = call.CallStatus() == 0 && connect_logo == 1)
             {
-                ret = sms.SendSMS("13169939943","Your house is on fire!");
+                ret = sms.SendSMS(TELEPHONE_NUMBER,"Your house is on fire!");
                 if ( ret == 1) 
                 {
                     printf("Send OK\n");
